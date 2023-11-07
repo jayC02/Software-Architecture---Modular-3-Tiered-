407af879ad5bbe814df08bd296cf316b756b3565
jayC02 <98602035+jayC02@users.noreply.github.com>
2023-11-07 16:30:01 +0000

    working version

---
+++ src/main/java/com/jays/destorecoursework/DeStoreCourseworkApplication.java
@@ -0,0 +1,15 @@
+package com.jays.destorecoursework;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DeStoreCourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeStoreCourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jays/destorecoursework/DeStoreCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jays.destorecoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeStoreCourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
